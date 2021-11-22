85871d212d10aa0d1ec9c748e58d110c7b45bd0a
NirVar <nvarsanno@gmail.com>
2021-11-22 17:32:01 +0200

    Initial commit

---
+++ src/test/BinaryExpression.java
@@ -0,0 +1,13 @@
+package test;
+
+public abstract class BinaryExpression implements Expression{
+    protected Expression left, right;
+
+    public BinaryExpression (Expression left, Expression right){
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public abstract double calculate();
+}
---
+++ src/test/Number.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Number implements Expression{
+    private double value;
+
+    public Number (double value){this.value= value;}
+
+    @Override
+    public double calculate() {
+        return value;
+    }
+}
